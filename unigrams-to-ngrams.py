27e2e7c2037465570a137e19e47af55cfdaaa6a2
Akarsh Simha <akarsh.simha@kdemail.net>
2014-04-20 20:19:55 -0500

    Script to convert unigrams to n-grams

---
+++ unigrams-to-ngrams.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+
+# 
+# Given MIDI file, with some essential details, write out the unigram state transitions
+#
+
+# Essential constants. See appropriate README to figure out how to adjust these
+FILE_NAME = "unigrams.dat";
+N_UNIGRAMS = 33;
+N_GRAMS = 2;
+
+# Compute state-space size
+N_STATES = N_UNIGRAMS**N_GRAMS;
+
+# print "Number of states:" + str(N_STATES)
+
+# Create state variable
+state = 0;
+
+# Read file
+f = open( FILE_NAME, 'r' );
+
+for line in f:
+    unigram = int( line );
+#    print "Unigram: " + str( unigram )
+    state = (state * N_UNIGRAMS + unigram) % N_STATES;
+    print state
