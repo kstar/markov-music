efca320fe95e2a41cf22675f27f3fbd700561ef7
Sivaramakrishnan S <ssivark007@gmail.com>
2014-04-20 20:17:18 -0500

    Model generator which takes input data and populates the transition matrix.
    Main part is one function to take a list of states and a placeholder for the matrix of transition probabilities and to populate the matrix and then normalize each row to sum to unity.
    There's also some code to read data from a file and then call the function on that input data.
    Some code used for testing is also pasted along at the bottom, and commented out.

---
+++ model-generator.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python2
+
+import numpy as np
+import scipy as sp
+
+
+
+# Function to read input file and increment corresponding elements of the matrix.
+
+def learn(inputdata,transprob):
+  	old_state = 1 # Too lazy to tell it what to do for the first value it reads. For some reason it seems to be okay even if I do not specify this -- but that would be bad handling of an indefinite edge case
+
+	for curr_state in np.nditer(inputdata):
+	  transprob[old_state,curr_state] = transprob[old_state,curr_state] + 1
+	  old_state = curr_state
+
+	# Normalizing each row to unity
+
+	for row in transprob:
+	  if (sum(row)!=0):
+	  	row /= sum(row)
+	
+	#rowsums = transprob.sum(axis=1)
+	#nonzero = rowsums > 0
+	#normtransprob = np.empty(transprob)
+	#transprob[nonzero] = transprob[nonzero] / rowsums[nonzero,np.newaxis] # Seems to be called broadcasting
+	#transprob = normtransprob
+
+# Sparse matrix for storing transition probabilities
+space_size = 10^2 # Can be made as an argument to the function later -- hardcoded N 
+transitionprob = np.zeros((space_size,space_size))
+
+# Actually readin the data from file
+trainingdata = np.fromfile("trainingmusic.dat",dtype=int,count=-1,sep="\n")
+
+# LEARNING
+
+learn(trainingdata,transitionprob)
+
+# Storing it as a sparse matrix -- ONLY IF THERE IS A NEED
+# sparse_transprob = sp.sparse.csr_matrix(transprob)
+
+#  -- TEST CODE -- 
+#b = np.random.randint(0,2,10000)
+#b = b.astype(int)
+#
+#np.savetxt('statelist.dat',b,fmt="%5i")
+#
+#samplemusic = np.fromfile("statelist.dat",dtype=int,count=-1,sep="\n")
+#
+#execfile("model-generator.py")
+
+
+
